e5a947c40d04e306cb8c5c290e26ac3f86134b87
Orion2675 <bryan.march@gmail.com>
2025-09-15 12:23:35 -0700

    Step 5. Update local_api.py with GET and POST commands. Interact with API

--- local_api.py
+++ local_api.py
@@ -3,12 +3,13 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+url = "http://127.0.0.1:8000"
+request = requests.get(url)
 
 # TODO: print the status code
-# print()
+print(request.status_code)
 # TODO: print the welcome message
-# print()
+print(request.text)
 
 
 
@@ -30,9 +31,10 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+p = url+"/data"
+post = request.post(p, json=data) 
 
 # TODO: print the status code
-# print()
+print(post.status_code)
 # TODO: print the result
-# print()
+print(post.text)
